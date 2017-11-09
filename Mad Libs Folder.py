3e9122e567ec713d58f75e1a740af41e5d283ca2
Bora48 <33519518+Bora48@users.noreply.github.com>
2017-11-09 09:12:01 -0500

    initial commit

---
+++ Mad Libs Folder.py
@@ -0,0 +1,66 @@
+### MAD LIBS ###
+### Source: https://i.pinimg.com/736x/4f/d2/fd/4fd2fdcd1a24611d0bbd238876096bbe--outer-space-camp-mad-libs-for-adults.jpg
+
+print("write an adjective")
+adj1 = input()
+
+print("write a noun")
+noun1 = input()
+
+print("write another adjective")
+adj2 = input()
+
+print ("write a plural noun")
+pnoun1 = input()
+
+print("write an adverb!")
+advrb1 = input()
+
+print("write a verb ending in ING")
+vrbendnining1 = input()
+
+print("write a silly word(plural)")
+sillyword1 = input()
+
+print("write another adjective!!!")
+adj3 = input()
+
+print("write a plural noun")
+pnoun2 = input()
+
+print("write any name")
+name1 = input()
+
+print("write an adjective")
+adj4 = input()
+
+print("write a number")
+numbr1 = input()
+
+print("write another name")
+name2 = input()
+
+print("write another name")
+name3 = input()
+
+print("write another name")
+name4 = input()
+
+print("write another name")
+name5 = input()
+
+print("write another name")
+name6 = input()
+
+print("write another name")
+name7 = input()
+
+print("last! write a plural noun")
+pnoun3 = input()
+
+### Begin Madlib ###
+
+print("When we look up into the sky on a/an" + adj1 + "summer night,we see millions of tiny spots of light.")
+
+print("Each one represents a/an" + noun1 + "which is the center of a/an" + adj2 + "solar system with dozens of" + pnoun1+ "revolving" + advrb1 + "around a distant sun.
+
